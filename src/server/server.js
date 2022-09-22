84fd23cd458b1688f2a9344fed85f00008c70d35
drummer3333 <drummer3333@users.noreply.github.com>
2022-09-23 00:56:34 +0200

    add static server

--- src/server/server.js
+++ src/server/server.js
@@ -12,14 +12,32 @@ osc.on('error', message => {
     console.log('error', message);
 });
 
-osc.on('*', message => {
-    console.log('got', message);
-});
+// osc.on('*', message => {
+//     console.log('got', message);
+// });
 
-osc.open(); // start a WebSocket server on port 8080
+osc.open();
 
 setInterval(function() {
     osc.send(new OSC.Message('/hi/v'));
 }, 4000);
 
+
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic('dist/midi', { index: ['index.html'] })
+
+// Create server
+var server = http.createServer(function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+})
+
+// Listen
+server.listen(4201)
+
 console.log('started');
+
+
